b083acd43a4627b4299a8ce16566c8119ef22a98
SoumyaKatta <soumya.katta@everest.engineering>
2025-03-12 22:52:07 +0530

    Create the Home screen

---
+++ aws_cloud_adventure/src/components/Home.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+function Home() {
+  const [image, setImage] = useState("");
+  const [previewUrl, setPreviewUrl] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+
+  const handleImageChange = (event:any) => {
+    const file = event.target.files && event.target.files[0]; // Check if a file is selected
+
+    if (file) {
+      setImage(file);
+      setPreviewUrl(URL.createObjectURL(file)); // Create a preview URL
+    }
+  };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     // TODO: Implement file upload logic
+//   };
+  return (
+    <>
+      <h1>Welcome to the Home screen</h1>
+      <label>
+        firstname: <input value={firstname} name="myfirstname" onChange={(e)=> setFirstname(e.target.value)} />
+      </label>
+      <hr />
+      <label>
+        Lastname: <input value={lastname} name="mylastname" onChange={(e)=> setFirstname(e.target.value)} />
+     </label>
+      <hr />
+      <label>
+        profile image: <input type="file" accept="image/*"  onChange={handleImageChange} />
+     </label>
+     {previewUrl && <img src={previewUrl} alt="Preview" />}
+
+    </>
+  );
+}
+export default Home;
