0935aaa33fb6d399563a67953436fa30234e1aec
SoumyaKatta <soumya.katta@everest.engineering>
2025-03-13 16:39:16 +0530

    Update the files

--- aws_cloud_adventure/src/App.tsx
+++ aws_cloud_adventure/src/App.tsx
@@ -5,6 +5,12 @@ function App() {
   return (
     <>
       <Home />
+      <video width={500} height={500} controls loop autoPlay muted>
+        <source
+          src="https://d3t8dlloq2ba6q.cloudfront.net/5124659_People_Person_3840x2160.mp4"
+          type="video/mp4"
+        />
+      </video>
     </>
   );
 }
--- aws_cloud_adventure/src/components/Home.tsx
+++ aws_cloud_adventure/src/components/Home.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+// import AWS from "aws-sdk";
+// const s3 = new AWS.S3();
 
 function Home() {
   const [image, setImage] = useState("");
@@ -6,7 +8,7 @@ function Home() {
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
 
-  const handleImageChange = (event:any) => {
+  const handleImageChange = (event: any) => {
     const file = event.target.files && event.target.files[0]; // Check if a file is selected
 
     if (file) {
@@ -14,27 +16,54 @@ function Home() {
       setPreviewUrl(URL.createObjectURL(file)); // Create a preview URL
     }
   };
+  // const handleSubmit = async (e: any) => {
+  //   e.preventDefault();
+  //   const fileName = e.target.files;
+  //   const params = {
+  //     Bucket: "my-bucket",
+  //     Key: fileName,
+  //     Body: e.target.files,
+  //     ACL: "public-read",
+  //   };
+  //   try {
+  //     // const uploadedFile = await s3.upload(params).promise();
+  //     console.log(uploadedFile);
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
 
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     // TODO: Implement file upload logic
-//   };
+  //   const handleSubmit = (event) => {
+  //     event.preventDefault();
+  //     // TODO: Implement file upload logic
+  //   };
   return (
     <>
       <h1>Welcome to the Home screen</h1>
       <label>
-        firstname: <input value={firstname} name="myfirstname" onChange={(e)=> setFirstname(e.target.value)} />
+        firstname:{" "}
+        <input
+          value={firstname}
+          name="myfirstname"
+          onChange={(e) => setFirstname(e.target.value)}
+        />
       </label>
       <hr />
       <label>
-        Lastname: <input value={lastname} name="mylastname" onChange={(e)=> setLastname(e.target.value)} />
-     </label>
+        Lastname:{" "}
+        <input
+          value={lastname}
+          name="mylastname"
+          onChange={(e) => setLastname(e.target.value)}
+        />
+      </label>
       <hr />
-      <label>
-        profile image: <input type="file" accept="image/*"  onChange={handleImageChange} />
-     </label>
-     {previewUrl && <img src={previewUrl} alt="Preview" />}
-
+        <label>
+          profile image:
+          <input type="file" accept="image/*" onChange={handleImageChange} />
+        </label>
+        <button type="submit">Upload</button>
+      {previewUrl && <img src={previewUrl} alt="Preview" />}
     </>
   );
 }
